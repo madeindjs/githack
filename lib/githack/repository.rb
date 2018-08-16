15cd3a812020e5cd2f58683a03af50d2e463f7f0
Rousseau Alexandre <contact@rousseau-alexandre.Fr>
2018-08-17 00:28:43 +0200

    add search_rails_config_database

--- lib/githack/repository.rb
+++ lib/githack/repository.rb
@@ -1,24 +1,57 @@
 require 'tmpdir'
+require 'yaml'
 require 'git'
 
 
 class Repository
 
-  attr_reader :git, :remote, :name
+  SENSIBLES_FILES = [
+    File.join('config', 'database.yml')
+  ]
+
+  attr_reader :git, :remote, :name, :path
 
   def initialize remote
     @remote = remote
     @name = remote.gsub /[^0-9A-Z]/i, '_'
+    @path = File.join(Dir.tmpdir, self.name)
 
-    if File.directory?(self.path)
-      @git = Git.open self.path
+    if File.directory? @path
+      @git = Git.open @path
     else
       @git = Git.clone @remote, @name, path: Dir.tmpdir
     end
   end
 
-  def path
-    return File.join(Dir.tmpdir, self.name)
+
+  def search_rails_config_database
+    results = []
+
+    @git.checkout 'master'
+
+    absolute_file_path = File.join @path, 'config', 'database.yml'
+
+
+    @git.log.object(absolute_file_path).each do |commit|
+      @git.checkout commit
+      next unless File.exists? absolute_file_path
+
+      begin
+        YAML.load(File.read(absolute_file_path)).each do |config|
+
+          config_name = config[0]
+          config_data = config[1]
+
+          next if config_data["adapter"] == "sqlite3"
+          results << config_data
+        end
+      rescue Psych::SyntaxError => e
+        
+      end
+
+    end
+
+    return results.uniq
   end
 
 
--- spec/githack/repository_spec.rb
+++ spec/githack/repository_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe Repository do
 
     expect(File.directory?(repository.path)).to be true
   end
+
+  it "should search_rails_config_database" do
+    repository = Repository.new 'https://github.com/RaspberryCook/website'
+
+    expect(repository.search_rails_config_database).to eq([{"adapter"=>"mysql2", "database"=>"raspberry_cook", "encoding"=>"utf8", "username"=>"raspberry_cook", "password"=>"secret", "host"=>"localhost", "pool"=>5, "timeout"=>5000}])
+  end
 end
