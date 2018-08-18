2ad9907a56913d93fefcd2ad79b4600e3c72c9bb
Rousseau Alexandre <contact@rousseau-alexandre.Fr>
2018-08-18 19:04:13 +0200

    Handle when file not exists

--- lib/githack/repository.rb
+++ lib/githack/repository.rb
@@ -2,25 +2,23 @@ require 'tmpdir'
 require 'yaml'
 require 'git'
 
-
 class Repository
-
   SENSIBLES_FILES = [
     File.join('config', 'database.yml')
-  ]
+  ].freeze
 
   attr_reader :git, :remote, :name, :path
 
-  def initialize remote
+  def initialize(remote)
     @remote = remote
     @name = remote.gsub /[^0-9A-Z]/i, '_'
-    @path = File.join(Dir.tmpdir, self.name)
+    @path = File.join(Dir.tmpdir, name)
 
-    if File.directory? @path
-      @git = Git.open @path
-    else
-      @git = Git.clone @remote, @name, path: Dir.tmpdir
-    end
+    @git = if File.directory? @path
+             Git.open @path
+           else
+             Git.clone @remote, @name, path: Dir.tmpdir
+           end
   end
 
   # Get all changements for config/secrets.yml file (who contains some API keys)
@@ -29,16 +27,12 @@ class Repository
 
     absolute_file_path = File.join @path, 'config', 'secrets.yml'
 
-    self.checkout_on_yaml_file_change(absolute_file_path) do |secrets|
-
-      puts secrets.inspect
+    checkout_on_yaml_file_change(absolute_file_path) do |secrets|
       results << secrets
-
     end
 
-    return results.uniq
+    results.uniq
   end
-
 
   # Get all changements for config/database.yml file (who contains database configuration for Ruby on Rails Framework)
   def search_rails_config_database
@@ -46,27 +40,25 @@ class Repository
 
     absolute_file_path = File.join @path, 'config', 'database.yml'
 
-    self.checkout_on_yaml_file_change(absolute_file_path) do |configs|
-
+    checkout_on_yaml_file_change(absolute_file_path) do |configs|
       configs.each do |config|
         config_data = config[1]
-        next if config_data["adapter"] == "sqlite3"
+        next if config_data['adapter'] == 'sqlite3'
         results << config_data
       end
-
     end
 
-    return results.uniq
+    results.uniq
   end
 
   # Checkout on all file changes
   #
   # @param file <String> absolute filepath
   # @yield <Hash> YAML file parsed
-  def checkout_on_yaml_file_change file
-    self.checkout_on_file_change(file) do
+  def checkout_on_yaml_file_change(file)
+    checkout_on_file_change(file) do
       begin
-        data = YAML.load(File.read(file))
+        data = YAML.safe_load(File.read(file))
         yield data
       rescue Psych::SyntaxError => e
         # can't parse YAML file
@@ -74,21 +66,22 @@ class Repository
     end
   end
 
-
   # Checkout on all file changes
   #
   # @param file <String> absolute filepath
   # @yield <Git::Log>
-  def checkout_on_file_change file
+  def checkout_on_file_change(file)
     @git.checkout 'master'
-    @git.log.object(file).each do |commit|
-      @git.checkout commit
-      next unless File.exists? file
+    begin
+      @git.log.object(file).each do |commit|
+        @git.checkout commit
+        next unless File.exist? file
 
-      yield commit
+        yield commit
+      end
+    rescue StandardError
+      Psych::BadAlias
     end
     @git.checkout 'master'
   end
-
-
 end
